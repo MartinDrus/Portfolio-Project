8c73caa908b329b45f2c124325d7e3325682ba9b
Martin <martindrus@outlook.de>
2022-08-26 11:09:16 +0200

    Added dropdown nav-bar with JS

--- js/script.js
+++ js/script.js
@@ -4,22 +4,32 @@
 let flags = document.querySelectorAll('.flags');
 
 // Fläche die geklickt werden muss, damit etwas passiert
-let clickArea = document.querySelector('#lang-select');
-let clickArea2 = document.querySelector('#lang-select2');
+let clickAreas = document.querySelectorAll('.lang-select');
+let clickAreas2 = document.querySelector('.drop');
 
 
-console.log(clickArea);
+let dropdown = document.querySelector('.menu-field');
 
-// clickHandler fuer die Sprachauswahl
-flags.forEach(function(item, index) {
-    clickArea.addEventListener('click', function(evt) {   
-    flags[index].classList.toggle('show-flags');
-    });
-});
 
-// clickHandler fuer die Sprachauswahl
-flags.forEach(function(item, index) {
-    clickArea2.addEventListener('click', function(evt) {   
-    flags[index].classList.toggle('show-flags');
-    });
-});
+let icons = document.querySelectorAll('.icon');
+
+
+/* Schleife läuft durch die Klickfelder  */
+ clickAreas.forEach(function(item) {
+    /* Beim Klick auf eine "Clickarea"/einem item in der forEach-Schleife 
+    werden über die zweite Schleife alle Flags getoggelt */
+    item.addEventListener('click', function(evt) {
+        flags.forEach(function(item){
+            item.classList.toggle('show-flags');
+        })
+    })
+ })
+
+
+icons.forEach (icon => {  
+   icon.addEventListener('click', (event) => {
+     icon.classList.toggle("open");
+     dropdown.classList.toggle("show");
+
+   });
+ });
