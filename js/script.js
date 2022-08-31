f0f7b03ba6f4a6826e6bf067336426693317c799
Martin <martindrus@outlook.de>
2022-08-31 08:58:22 +0200

    - Added Second Level DropDown - Changed Color/Styles - Modified Responsivness

--- js/script.js
+++ js/script.js
@@ -5,13 +5,12 @@ let flags = document.querySelectorAll('.flags');
 
 // Fläche die geklickt werden muss, damit etwas passiert
 let clickAreas = document.querySelectorAll('.lang-select');
-let clickAreas2 = document.querySelector('.drop');
-
+let clickGlobe = document.querySelectorAll('.dropdown-trigger-globe');
 
 let dropdown = document.querySelector('.menu-field');
-
-
+let dropdown2 = document.querySelector('.second-level-dropdown');
 let icons = document.querySelectorAll('.icon');
+
 
 
 /* Schleife läuft durch die Klickfelder  */
@@ -25,6 +24,13 @@ let icons = document.querySelectorAll('.icon');
     })
  })
 
+ /* Der ClickListener für die Sprachauswahl DropDown des Headers */
+clickGlobe.forEach(globe => {
+    globe.addEventListener('click', (event) => {
+        dropdown2.classList.toggle("show-drop");
+        dropdown.classList.toggle("show-drop");
+    })
+})
 
 icons.forEach (icon => {  
    icon.addEventListener('click', (event) => {
@@ -33,3 +39,27 @@ icons.forEach (icon => {
 
    });
  });
+
+
+
+let settime;
+document.getElementById('imgchange').addEventListener("mouseover", imgchanges);
+document.getElementById('imgchange').addEventListener("mouseout", outtime);
+
+function imgchanges(){
+document.getElementById('my-pics').src = "../../assets/img/MartinDrus2.webp";
+var myArray = ['../../assets/img/MartinDrus.webp', '../../assets/img/MartinDrus2.webp'];
+var count = 2;
+settime = setInterval(function() {
+  if(count >= myArray.length){
+  count = 0;
+  }
+  var rand = myArray[count];
+  document.getElementById('my-pics').src = rand;
+  count++;
+}, 1200);
+}
+function outtime(){
+clearInterval(settime);
+document.getElementById('my-pics').src = "../../assets/img/MartinDrus.webp";
+}
