35db8f9b98927cf6685dd4e9230df00cdb6e0d66
Martin <martindrus@outlook.de>
2022-11-09 09:07:10 +0100

    changed background

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ let dropdown = document.querySelector('.menu-field');
 let dropdown2 = document.querySelector('.second-level-dropdown');
 let icons = document.querySelectorAll('.icon');
 
+//Time
+let timeContainer = document.querySelector("#time-container")
+
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 
 /* Schleife läuft durch die Klickfelder  */
@@ -41,7 +47,7 @@ icons.forEach (icon => {
  });
 
 
-
+//! Picture change
 let settime;
 document.getElementById('imgchange').addEventListener("mouseover", imgchanges);
 document.getElementById('imgchange').addEventListener("mouseout", outtime);
@@ -63,3 +69,36 @@ function outtime(){
 clearInterval(settime);
 document.getElementById('my-pics').src = "../../assets/img/MartinDrus.webp";
 }
+
+// ! Time
+
+function digitalDate() {
+  let now = new Date();
+  timeContainer.style = ("margin-left: 150px")
+  timeContainer.innerHTML = now;
+
+}
+setInterval(digitalDate, 1000);
+
+
+
+
+function setClock() {
+let rightNow = new Date();
+
+const seconds = rightNow.getSeconds();
+const secondsDegrees = ((seconds / 60) * 360) + 90;
+secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+const mins = rightNow.getMinutes();
+const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+const hour = rightNow.getHours();
+const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+
+setInterval(setClock, 1000);
+
